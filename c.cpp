40bfbacc9e1c8b11a03a7e73244d7821e2c3b000
Lucifer-1811 <neerajnanda05@gmail.com>
2024-06-11 14:40:30 +0530

    2ND DAYS CLASS ALL CODES

---
+++ c.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+  cout << "Hello World!"<<endl;
+int a=40;
+float b=5;
+int e;
+double c=9;
+char d='chao';
+cout<<sizeof(a);
+cout<<sizeof(b);
+cout<<sizeof(c);
+cout<<sizeof(d)<<endl;
+cout<<a+e<<endl;
+
+
+  return 0;
+}
---
+++ day2.cpp
@@ -0,0 +1,86 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    //arithemtic operator
+    // + - * / %
+    //assign operstor
+    //= += -=
+    // relational operators
+    // ==  != > >= < <=
+///////////////////////////////////////////////////////////
+/*    
+int a;
+int b;
+    cout<<"enter two number"<<endl;
+    cin>>a>>b;
+
+    cout<<a+b<<endl;
+*/
+
+//////////////////////////////////////////////////////////
+/*
+int sub1,sub2,sub3;
+
+cout<<"enter sub1 mark"<<endl;
+cin>>sub1;
+cout<<"enter sub2 mark"<<endl;
+cin>>sub2;
+cout<<"enter sub3 mark"<<endl;
+cin>>sub3;
+
+
+double p=(sub1+sub2+sub3/300)*100;
+
+if (p>=33)
+{
+    cout<<"pass hai bhai"<<endl;
+}
+else{
+    cout<<"fail ho gya bhai koi  naa agli baar padh ke aaana chandan"<<endl;
+}
+
+
+
+/////////////////////////////////////////////////////////////////////////////////
+
+//loop
+int n;
+cin>>n;
+for(int i=0;i<=n;i++){
+
+
+    for( int j=n;j>=i;j--){
+        cout<<"*";
+    }
+    cout<<""<<endl;
+}
+
+
+double arr[]={1.6,3.2,4,6};
+for (auto item : arr)
+{
+    cout<<item<<" ";
+
+}
+
+////////////////////////////////////////////////////////////
+int marks;
+cout<<"emter the mark";
+cin>>marks;
+
+string result=(marks >= 40)? "pass" :"fail";
+cout<<result;
+
+*/
+
+int num=0;
+
+string result=(num ==0) ? "zero" : (num>0)? "positive" :"negative";
+
+
+
+
+return 0;
+
+}
