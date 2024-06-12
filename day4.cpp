29dc251cbf52a26f7575a57ef3a41c090d6078b0
Lucifer-1811 <neerajnanda05@gmail.com>
2024-06-12 15:47:30 +0530

    updated

--- day4.cpp
+++ day4.cpp
@@ -1,56 +1,83 @@
-// /////////////////////////////////////////class///////////////////////
+// // /////////////////////////////////////////class///////////////////////
+// // // #include<iostream>
+// // // using namespace std;
+
+// // // class calculation{
+// // //     public:
+// // //     int length;
+// // //     int height;
+// // //     int breadth;
+
+// // //     int calculatearea(){
+// // //         return length*breadth;
+
+// // //     }
+// // //      int calculatevolume(){
+// // //         return length*breadth*height;
+
+// // //     }
+// // // };
+// // // int main(){
+// // //      calculation c1;
+// // //      c1.length=10;
+// // //      c1.height=5;
+// // //      c1.breadth=7;
+
+// // //      cout<<c1.calculatearea()<<endl;
+
+// // //      cout<<c1.calculatevolume()<<endl;
+
+// // //   return 0
+// // // }
+
+// // ////////////////////////////////////////////////////////////////////constructor
+// // //a specaail mebmber function which is called automatically when an object is created,
+// // // this how we created.it have same classname and no return type
+
+
+// // //default constructor::
+
 // // #include<iostream>
 // // using namespace std;
 
-// // class calculation{
+// // class emp{
 // //     public:
-// //     int length;
-// //     int height;
-// //     int breadth;
-
-// //     int calculatearea(){
-// //         return length*breadth;
-
-// //     }
-// //      int calculatevolume(){
-// //         return length*breadth*height;
-
-// //     }
-// // };
-// // int main(){
-// //      calculation c1;
-// //      c1.length=10;
-// //      c1.height=5;
-// //      c1.breadth=7;
-
-// //      cout<<c1.calculatearea()<<endl;
-
-// //      cout<<c1.calculatevolume()<<endl;
-
-// //   return 0
+// //         emp(){
+// //             cout<<"constructor called"<<endl;
+// //         }
 // // }
 
-// ////////////////////////////////////////////////////////////////////constructor
-// //a specaail mebmber function which is called automatically when an object is created,
-// // this how we created.it have same classname and no return type
+// // int main()
+// // {
+// //     emp e1;
+// //  return 0;
+// // }
 
-
-// //default constructor::
+// // //////////////////////////////////////////////////////////////////////////////
 
 // #include<iostream>
 // using namespace std;
-
-// class emp{
+// class employee{
 //     public:
-//         emp(){
-//             cout<<"constructor called"<<endl;
-//         }
+//     int id;
+//     string name;
+//     double sal;
+//     employee(){
+//         name="Neeraj kumar mahananda";
+//         sal=1000000;
+//         id=007;
 // }
+// void print(){
+//     cout<<"Employee id is :"<<id<<endl;
+//     cout<<"Employee name is :"<<name<<endl;
+//     cout<<"salary is :"<<sal<<endl;
 
+
+// }
+// };
 // int main()
 // {
-//     emp e1;
+//     employee e1;
+//     e1.print();
 //  return 0;
 // }
-
-// //////////////////////////////////////////////////////////////////////////////
