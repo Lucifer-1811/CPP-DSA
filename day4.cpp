343e7c9a3e637ae216aa04d97982c3965fd0ffb4
Lucifer-1811 <neerajnanda05@gmail.com>
2024-06-12 15:36:48 +0530

    day4

---
+++ day4.cpp
@@ -0,0 +1,56 @@
+// /////////////////////////////////////////class///////////////////////
+// // #include<iostream>
+// // using namespace std;
+
+// // class calculation{
+// //     public:
+// //     int length;
+// //     int height;
+// //     int breadth;
+
+// //     int calculatearea(){
+// //         return length*breadth;
+
+// //     }
+// //      int calculatevolume(){
+// //         return length*breadth*height;
+
+// //     }
+// // };
+// // int main(){
+// //      calculation c1;
+// //      c1.length=10;
+// //      c1.height=5;
+// //      c1.breadth=7;
+
+// //      cout<<c1.calculatearea()<<endl;
+
+// //      cout<<c1.calculatevolume()<<endl;
+
+// //   return 0
+// // }
+
+// ////////////////////////////////////////////////////////////////////constructor
+// //a specaail mebmber function which is called automatically when an object is created,
+// // this how we created.it have same classname and no return type
+
+
+// //default constructor::
+
+// #include<iostream>
+// using namespace std;
+
+// class emp{
+//     public:
+//         emp(){
+//             cout<<"constructor called"<<endl;
+//         }
+// }
+
+// int main()
+// {
+//     emp e1;
+//  return 0;
+// }
+
+// //////////////////////////////////////////////////////////////////////////////
